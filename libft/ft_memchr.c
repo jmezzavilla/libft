984b3bbe683a710887f878fb77ddfe1d87af3f9d
Jessica Mezzavilla <jessicamezzavilla@gmail.com>
2023-03-11 17:11:24 +0000

    new

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,40 @@
 /*   By: jmezzavilla <jmezzavilla@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:59:06 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/03/07 13:59:43 by jmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/03/11 15:43:29 by jmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *p;
+    size_t count;
+
+    p = s;
+    count = 0;
+
+    while(n--)
+    {
+        if(p[count] == c)
+        {
+            return((void *) (p + count));
+        }
+        count++;
+    }
+    return(0);
+}
+
+/*int main()
+{
+    char str[] = "jessica";
+    char *ptr;
+    char *ptr2;
+
+    ptr = ft_memchr(str, 'i', ft_strlen(str));
+    printf("%s\n", ptr);
+
+    ptr2 = memchr(str, 'i', ft_strlen(str));
+    printf("%s", ptr2);
+}*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jmezzavilla <jmezzavilla@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 17:48:10 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/03/11 13:57:05 by jmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/03/11 15:51:45 by jmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,13 @@ int ft_atoi(const char *nptr);
 void ft_bzero(void *s, size_t n);
 
 char	*ft_strdup(const char *s);
+void *ft_calloc(size_t nmemb, size_t size);
 
 void *ft_memset(void *s, int c, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+void *ft_memchr(const void *s, int c, size_t n);
 
 
 int ft_isalpha(int c);
