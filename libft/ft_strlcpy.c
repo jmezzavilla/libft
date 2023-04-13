5ef13cacbb451f4e0044cbdffb0b4d610fcce7f0
Victor Mezzavilla <vmezavilla@gmail.com>
2023-04-13 20:00:05 +0100

    fix test arcnet

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vmezzavilla <vmezzavilla@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:54:21 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/03/11 19:20:06 by vmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/04/13 19:32:39 by vmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t	count;
+	size_t count;
 
 	count = 0;
+	if (size == 0)
+		return (ft_strlen(src));
 	if (size != 0)
 	{
 		while (count < (size - 1) && src[count] != '\0')
--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: vmezzavilla <vmezzavilla@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:59:22 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/03/11 19:40:03 by vmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/04/13 19:57:24 by vmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	count;
+	unsigned int	i;
+	char			*last_occurence;
 
-	count = ft_strlen(s);
-	if (c == '\0')
-		return ((char *)(s + count));
-	while (count--)
+	last_occurence = NULL;
+	i = 0;
+	while (s[i])
 	{
-		if (s[count] == (char)c)
-		{
-			return ((char *)(s + count));
-		}
+		if (s[i] == (char)c)
+			last_occurence = (char *)&s[i];
+		i++;
 	}
-	return (NULL);
+	if ((char)c == s[i])
+		return ((char *)&s[i]);
+	return (last_occurence);
 }
-
-/*int	main(void)
-{
-	const char str[] = "Jasmim Jéssica.";
-	const char target = '\0';
-
-	printf("%s", ft_strrchr(str, target));
-	printf("%s", strrchr(str, target));
-}*/
