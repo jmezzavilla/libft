9cc89501a4ecb8b996fca7a21ef8feab3a876af7
Jessica Mezzavilla <jessicamezzavilla@gmail.com>
2023-04-15 21:45:20 +0100

    split finish

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -2,90 +2,105 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vmezzavilla <vmezzavilla@student.42.fr>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                    +:+ +:+        
+	+:+     */
+/*   By: jmezzavilla <jmezzavilla@student.42.fr>    +#+  +:+      
+	+#+        */
+/*                                                +#+#+#+#+#+  
+	+#+           */
 /*   Created: 2023/03/11 17:50:45 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/04/13 23:36:59 by vmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/04/13 23:36:59 by jmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_count_words(char const *s, char c)
+static int	unleah(char **str, int size)
 {
-	int	words;
+	while (size--)
+		free(str[size]);
+	return (-1);
+}
 
-	words = 1;
-	if (s)
+int	ft_count_words(char const *str, char c)
+{
+	int i;
+	int words;
+
+	words = 0;
+	i = 0;
+	while (str[i] != '\0')
 	{
-		while (*s)
-		{
-			if (*s == c)
-				words++;
-			s++;
-		}
+		if ((str[i + 1] == c || str[i + 1] == '\0') == 1 && (str[i] == c
+				|| str[i] == '\0') == 0)
+			words++;
+		i++;
 	}
 	return (words);
 }
 
-void	ft_write_words(char **split, char const *str, char c)
+int	ft_aloc_memory(char **split, int word, size_t size)
 {
-	const char	*start;
-	const char	*p;
-	int			word;
-	int			len;
-
-	start = str;
-	word = 0;
-	while (*str)
+	split[word] = (char *)ft_calloc(size, sizeof(char));
+	if (!split[word])
 	{
-		if ((*str == c || *str == '\0'))
-		{
-			str++;
-			start = str;
-		}
+		return (unleah(split, word - 1));
+	}
+}
+
+int	ft_write_words(char **split, char const *str, char c)
+{
+	int i;
+	int j;
+	int word;
+
+	word = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] == c || str[i] == '\0') == 1)
+			i++;
 		else
 		{
-			p = start;
-			while (*p != '\0' && *p != c)
-			{
-				p++;
-			}
-			len = p - start;
-			split[word] = (char *)ft_calloc(len + 1, sizeof(char));
-			ft_strlcpy(split[word], start, len + 1);
-			split[word][len + 1] = '\0';
-			str = p;
+			j = 0;
+			while ((str[i + j] == c || str[i + j] == '\0') == 0)
+				j++;
+			ft_aloc_memory(split, word, j + 1);
+			ft_strlcpy(split[word], str + i, j + 1);
+			i += j;
 			word++;
 		}
 	}
+	return (0);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**res;
+	char **res;
 
-	res = (char **)ft_calloc(ft_count_words(s, c), sizeof(char *));
+	res = (char **)ft_calloc(ft_count_words(s, c) + 1, sizeof(char *));
 	if (!res)
 		return (NULL);
-	ft_write_words(res, s, c);
+	if (ft_write_words(res, s, c) != 0)
+	{
+		return (NULL);
+	}
 	return (res);
 }
 
 /*int	main(void)
 {
-	char			**tab;
-	unsigned int	i;
+	char **tab;
+	unsigned int i;
 
 	i = 0;
-	tab = ft_split("Ola meu nome é Jessica ", ' ');
+	tab = ft_split("ola meu nome e jessica", ' ');
 	if (!tab[0])
 		ft_putstr_fd("ok\n", 1);
 	while (tab[i] != NULL)
 	{
 		ft_putstr_fd(tab[i], 1);
-		write(2,"\n",1);
+		write(2, "\n", 1);
 		i++;
 	}
 }*/
