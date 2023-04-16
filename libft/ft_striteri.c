fe057d0d8c99e546485831dc74d7c4b1728b5690
Jessica Mezzavilla <jessicamezzavilla@gmail.com>
2023-04-16 23:37:34 +0100

    fix striteri

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: jmezzavilla <jessicamezzavilla@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 17:51:29 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/04/16 23:02:44 by jmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/04/16 23:37:21 by jmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
-	if (s)
+	i = 0;
+	while (s[i] != 0)
 	{
-		i = 0;
-		while (*(s + i) && f)
-		{
-			f(i, s + i);
-			i++;
-		}
+		f(i, &s[i]);
+		i++;
 	}
 }
