e5851153ec05d1e27bf3dd941b24af0e1937f550
jmezzavilla <jessicamezzavilla@gmail.com>
2023-07-29 21:22:48 +0100

    Add strlen **

--- str/ft_strlen_matrix.c
+++ str/ft_strlen_matrix.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strlen_matrix.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmezzavilla <jessicamezzavilla@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:50:56 by jmezzavilla       #+#    #+#             */
-/*   Updated: 2023/07/29 20:52:05 by jmezzavilla      ###   ########.fr       */
+/*   Updated: 2023/07/29 20:55:03 by jmezzavilla      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen_matrix(char **str)
 {
 	size_t	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
+	while (str[i])
 		i++;
-	}
 	return (i);
 }
