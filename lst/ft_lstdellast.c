6f6b016960bb614667239a9fb786e75753eadc99
Jessica Mezzavilla <jessicamezzavilla@gmail.com>
2023-09-30 10:29:45 +0100

    norminette

--- lst/ft_lstdellast.c
+++ lst/ft_lstdellast.c
@@ -6,7 +6,7 @@
 /*   By: jealves- <jealves-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 21:11:12 by jealves-          #+#    #+#             */
-/*   Updated: 2023/09/26 16:28:10 by jealves-         ###   ########.fr       */
+/*   Updated: 2023/09/30 10:25:57 by jealves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstdellast(t_list **lst)
 {
 	t_list	*current;
 
-	current = ft_lstidx(*lst ,ft_lstsize(*lst) - 2);
+	current = ft_lstidx(*lst, ft_lstsize(*lst) - 2);
 	free(current->next);
 	current->next = NULL;
 }
