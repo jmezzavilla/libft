a25a04de8ba19d590c56b28d116e240937ca57ad
Jessica Mezzavilla <jessicamezzavilla@gmail.com>
2023-10-05 00:23:10 +0100

    fix numbers

--- nbr/ft_atoi.c
+++ nbr/ft_atoi.c
@@ -6,21 +6,40 @@
 /*   By: jealves- <jealves-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 22:34:14 by jealves-          #+#    #+#             */
-/*   Updated: 2023/10/05 00:05:54 by jealves-         ###   ########.fr       */
+/*   Updated: 2023/10/05 00:15:31 by jealves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_result_atoi(const char *nptr, int sign, int i)
+{
+	int	result;
+
+	result = 0;
+	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		if ((result > INT_MAX / 10) || (result == INT_MAX / 10 && (nptr[i]
+					- '0') > (INT_MAX % 10)))
+		{
+			if (sign == -1)
+				return (INT_MIN);
+			else
+				return (INT_MAX);
+		}
+		result = (result * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	int	i;
 	int	sign;
-	int	result;
 
 	i = 0;
 	sign = 1;
-	result = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-')
@@ -30,19 +49,7 @@ int	ft_atoi(const char *nptr)
 	}
 	else if (nptr[i] == '+')
 		i++;
-	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		if ((result > INT_MAX / 10) || 
-            (result == INT_MAX / 10 && (nptr[i] - '0') > (INT_MAX % 10))) {
-            if(sign == -1)
-				return (INT_MIN);
-			else
-            	return (INT_MAX);
-        }
-		result = (result * 10) + (nptr[i] - '0');
-		i++;
-	}
-	return (result * sign);
+	return (ft_result_atoi(nptr, sign, i));
 }
 
 /*int main(void)
--- nbr/ft_atoi_long.c
+++ nbr/ft_atoi_long.c
@@ -6,21 +6,40 @@
 /*   By: jealves- <jealves-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 22:34:14 by jealves-          #+#    #+#             */
-/*   Updated: 2023/10/05 00:05:50 by jealves-         ###   ########.fr       */
+/*   Updated: 2023/10/05 00:16:32 by jealves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+long	ft_result_atoi_long(const char *nptr, int sign, int i)
+{
+	long	result;
+
+	result = 0;
+	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		if ((result > LONG_MAX / 10) || (result == LONG_MAX / 10 && (nptr[i]
+					- '0') > (LONG_MAX % 10)))
+		{
+			if (sign == -1)
+				return (LONG_MIN);
+			else
+				return (LONG_MAX);
+		}
+		result = (result * 10) + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
 long	ft_atoi_long(const char *nptr)
 {
 	long	i;
 	long	sign;
-	long	result;
 
 	i = 0;
 	sign = 1;
-	result = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '-')
@@ -30,19 +49,7 @@ long	ft_atoi_long(const char *nptr)
 	}
 	else if (nptr[i] == '+')
 		i++;
-	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
-	{
-		if ((result > LONG_MAX / 10) || 
-            (result == LONG_MAX / 10 && (nptr[i] - '0') > (LONG_MAX % 10))) {
-			if(sign == -1)
-				return (LONG_MIN);
-			else
-            	return (LONG_MAX);
-        }
-		result = (result * 10) + (nptr[i] - '0');
-		i++;
-	}
-	return (result * sign);
+	return (ft_result_atoi_long(nptr, sign, i));
 }
 
 /*int main(void)
