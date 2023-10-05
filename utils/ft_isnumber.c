945f950fc42b5b0592d9374471606a23c71b7ef4
Jessica Mezzavilla <jessicamezzavilla@gmail.com>
2023-10-05 23:22:45 +0100

    norminette

--- utils/ft_isnumber.c
+++ utils/ft_isnumber.c
@@ -6,7 +6,7 @@
 /*   By: jealves- <jealves-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 22:43:16 by jealves-          #+#    #+#             */
-/*   Updated: 2023/10/05 16:36:06 by jealves-         ###   ########.fr       */
+/*   Updated: 2023/10/05 23:18:23 by jealves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@ int	ft_isnumber(char *str)
 	int	i;
 
 	i = 0;
-	
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-		
-	if(str[i] == '\0')
+	if (str[i] == '\0')
 		return (0);
-		
 	while (str[i])
 	{
 		if (!ft_isdigit(str[i]))
